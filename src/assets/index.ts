92eefe7d5999df6e19270fc947512eddc74f8db0
Jairo Rocha <jairochabr@gmail.com>
2024-12-10 12:51:56 -0300

    chore: add images in index.ts

---
+++ src/assets/index.ts
@@ -0,0 +1,9 @@
+import cursor from './images/cursor.png';
+import madeByLogo from './images/logo.png';
+import message from './images/message.png';
+
+export {
+  cursor,
+  madeByLogo,
+  message
+}
