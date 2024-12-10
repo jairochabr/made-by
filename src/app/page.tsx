bc570252b6a3284589626b1ddfe4b8c85b23637c
Jairo Rocha <jairochabr@gmail.com>
2024-12-10 12:54:10 -0300

    feat: add the Header and Hero components to the Home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,13 @@
 import { Banner } from "@/components/Banner";
+import { Header } from "@/components/Header";
+import { Hero } from "@/components/Hero";
 
 export default function Home() {
   return (
     <>
       <Banner />
+      <Header />
+      <Hero />
     </>
   );
 }
