0b93917ee186f4053f5aceea469033120eddcdcd
Jairo Rocha <jairochabr@gmail.com>
2024-12-12 14:53:45 -0300

    chore: add imagens do Partners of the constants

---
+++ src/constantes/index.ts
@@ -0,0 +1,34 @@
+import { acme, apex, celestia, echo, pulse, quantum } from "@/assets";
+
+export const PARTNERS = [
+  {
+    id: "1",
+    src: acme,
+    alt: "Acme Corp Logo",
+  },
+  {
+    id: "2",
+    src: quantum,
+    alt: "Quantum Logo",
+  },
+  {
+    id: "3",
+    src: echo,
+    alt: "Echo Valley Logo",
+  },
+  {
+    id: "4",
+    src: celestia,
+    alt: "Celestia Logo",
+  },
+  {
+    id: "5",
+    src: pulse,
+    alt: "Pulse Logo",
+  },
+  {
+    id: "6",
+    src: apex,
+    alt: "Apex Logo",
+  },
+];
