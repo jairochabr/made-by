e041b55e42a4ba7f24098fbde48b1909d3e92a0b
Jairo Rocha <jairochabr@gmail.com>
2024-12-12 14:52:56 -0300

    chore: add Partners component of the Home

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,7 @@
 import { Banner } from "@/components/Banner";
 import { Header } from "@/components/Header";
 import { Hero } from "@/components/Hero";
+import { Partners } from "@/components/Partners";
 
 export default function Home() {
   return (
@@ -8,6 +9,7 @@ export default function Home() {
       <Banner />
       <Header />
       <Hero />
+      <Partners />
     </>
   );
 }
