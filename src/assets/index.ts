71cb4b044c2901fa101873df8f2113428aacd194
Jairo Rocha <jairochabr@gmail.com>
2024-12-12 14:51:45 -0300

    feat: add imagens of the Partners

--- src/assets/index.ts
+++ src/assets/index.ts
@@ -2,8 +2,21 @@ import cursor from './images/cursor.png';
 import madeByLogo from './images/logo.png';
 import message from './images/message.png';
 
+import acme from './partners/acme.png';
+import apex from './partners/apex.png';
+import celestia from './partners/celestia.png';
+import echo from './partners/echo.png';
+import pulse from './partners/pulse.png';
+import quantum from './partners/quantum.png';
+
 export {
   cursor,
   madeByLogo,
-  message
+  message,
+  acme,
+  apex,
+  celestia,
+  echo,
+  pulse,
+  quantum
 }
