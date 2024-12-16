31c10b8f6259df0ee06553aa3e083de527c3c3ed
Jairo Rocha <jairochabr@gmail.com>
2024-12-16 20:31:05 -0300

    chore: adjust the component

---
+++ src/components/icons/index.ts
@@ -0,0 +1,12 @@
+export { ArrowRight } from "./other/arrow-right";
+export { Leaf } from "./other/leaf";
+export { Menu } from "./other/menu";
+export { Minus } from "./other/minus";
+export { Plus } from "./other/plus";
+
+export { Instagram } from "./socials/instagram";
+export { LinkedIn } from "./socials/linkedIn";
+export { Pinterest } from "./socials/pinterest";
+export { Tiktok } from "./socials/tiktok";
+export { X } from "./socials/x";
+export { Youtube } from "./socials/youtube";
