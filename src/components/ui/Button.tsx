fca99b5f4e5cc7404b147815941608ab08b7dee9
Jairo Rocha <jairochabr@gmail.com>
2024-12-16 16:12:31 -0300

    refactor: code improvements on the Button component

--- src/components/ui/Button.tsx
+++ src/components/ui/Button.tsx
@@ -1,6 +1,37 @@
-export function Button({ children, ...props }) {
+import { ButtonHTMLAttributes, ReactNode } from "react";
+import { twMerge } from "tailwind-merge";
+import { tv } from "tailwind-variants";
+
+interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
+  children: ReactNode;
+  color?: "primary" | "secondary";
+  size?: "small" | "large";
+}
+
+export function Button({ children, color = "primary", size = "small", ...rest}: ButtonProps) {
+  const button = tv({
+    base: "rounded-lg font-secondary font-medium",
+    variants: {
+      color: {
+        primary: "bg-white text-black transition-all duration-300 hover:text-brand-secondary",
+        secondary: "text-white transition-all duration-300 hover:bg-gray-800",
+      },
+      size: {
+        small: "px-4 py-2",
+        large: "px-5 py-4",
+      },
+    },
+    defaultVariants: {
+      color: "primary",
+      size: "small",
+    },
+  });
+
   return (
-    <button className="rounded-lg font-secondary bg-white px-4 py-2 font-medium" {...props}>
+    <button
+      {...rest}
+      className={twMerge(button({ color, size }), rest.className)}
+    >
       {children}
     </button>
   );
